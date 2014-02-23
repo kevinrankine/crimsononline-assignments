11f554232b8d390e0b44dae94bc2658b7fc6014e
Kevin Akos Rankine <rankine@college.harvard.edu>
2014-02-22 20:01:27 -0500

    No tests yet

--- assignment1/question2.py
+++ assignment1/question2.py
@@ -1,3 +1,10 @@
+import json
+import Image
+
+class Callable:
+    def __init__(self, c):
+        self.__call__ = c
+
 class Article:
     '''
     Question 2a
@@ -12,7 +19,7 @@ class Article:
     Question 2b
         Methods:
             - Load article from text file
-
+    I used http://code.activestate.com/recipes/52304-static-methods-aka-class-methods-in-python/ for this
     Question 2d
         Properties:
             - related_image
@@ -21,7 +28,34 @@ class Article:
             - modify load to load info about related picture (if it exists)
             - modify show to also show the related picture (if it exist)
     '''
-    pass
+    def __init__(self, headline, content, creator, related_image = None):
+        self.headline = headline
+        self.content = content
+        self.creator = creator
+        self.related_image = related_image
+    
+    def fromFile(filename):
+        rep = open(filename, 'r')
+        rep = json.load(rep)
+        return Article(rep["headline"], rep["content"], rep["creator"], rep["related_image"])
+
+    fromFile = Callable(fromFile)
+        
+    def show(self):
+        print self.headline + "\n"
+        print "By: " + self.creator + "\n"
+        print self.content
+        
+    
+    def save(self, filename):
+        f = open(filename, 'w')
+        text = json.dumps({"headline" : self.headline, 
+                           "creator" : self.creator,
+                           "content" : self.content,
+                           "related_image" : self.related_image})
+        f.write(text)
+        
+        
 
 class Picture:
     '''
@@ -32,4 +66,8 @@ class Picture:
          Methods
             - show (show image)
     '''
-    pass
+    def __init__(self, path, creator):
+        self.path = path
+        self.creator = creator
+    def show(self):
+        Image.open(self.path).show()
