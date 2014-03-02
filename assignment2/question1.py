e5d2cff659ac953aa202fe4e2bd6adb1e1462636
Kevin Akos Rankine <rankine@college.harvard.edu>
2014-03-02 16:11:18 -0500

    init

--- assignment2/question1.py
+++ assignment2/question1.py
@@ -1,3 +1,9 @@
+import datetime
+import re
+import Image
+
+
+
 class Content(object):
     '''
     Required properties:
@@ -9,7 +15,57 @@ class Content(object):
         - show
         - matches_url (question 1d)
     '''
-    pass
+    def __init__(self, title, subtitle, creator, pub_date):
+        self.title = title
+        self.subtitle = subtitle
+        self.creator = creator
+        self.pub_date = pub_date
+
+    def show(self):
+        print "Title: {0}\nSubtitle: {1}\nCreator: {2}\nPublished on: {3}".format(self.title, self.subtitle, self.creator, self.pub_date.strftime("%Y/%m/%d"))
+
+    def fix(self, c):
+        if len(c) == 1:
+            return '0' + c
+        else:
+            return c
+            
+    def matches_url(self, url):
+        p = "http://www.thecrimson.com/(\w+)/(\\d+)/(\\d+)/(\\d+)/(.+)/"
+        m = re.match(p, url)
+        if not m:
+            return 
+        else:
+            content_type = m.group(1)
+            year = m.group(2)
+            month = self.fix(m.group(3))
+            day = self.fix(m.group(4))
+            slug = m.group(5)
+            if self.pub_date.strftime("%Y/%m/%d") == "{0}/{1}/{2}".format(year, month, day):
+                if type(self) is Article and content_type == "article" and self.title == slug:
+                    return True
+                elif type(self) is Picture and content_type == "image" and self.title == slug:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+    @classmethod
+    def from_url(cls, l_con, url):
+        l_con = filter(lambda c : c.matches_url(url), l_con)
+        if len(l_con) == 0:
+            return None
+        elif len(l_con) == 1:
+            return l_con[0]
+        else:
+            print "Duplicates in Content list"
+            return None
+    @classmethod
+    def posted_after(cls, l_con, t):
+        return filter(lambda c : c.pub_date - t > datetime.timedelta(0), l_con)
+    
+                    
+            
 
 class Article(Content):
     '''
@@ -19,7 +75,16 @@ class Article(Content):
     Required methods:
         - All methods of Content
     '''
-    pass
+    def __init__(self, title, subtitle, creator, pub_date, related_image = None):
+        super(Article, self).__init__(title, subtitle, creator, pub_date)
+        self.related_image = related_image
+
+    def show(self):
+        super(Article, self).show()
+        if self.related_image:
+            Image.open(self.related_image.path)
+    
+        
 
 class Picture(Content):
     '''
@@ -29,16 +94,9 @@ class Picture(Content):
     Required methods:
         - All methods of Content
     '''
-    pass
-
-'''
-Question 1e
-'''
-def from_url(c_lst, url):
-    pass
-
-'''
-Question 1e
-'''
-def posted_after(c_lst, dt):
-    pass
+    def __init__(self, title, subtitle, creator, pub_date, image_file):
+        super(Picture, self).__init__(title, subtitle, creator, pub_date)
+        self.image_file = image_file
+        
+    def show(self):
+        Image.open(self.image_file).show()
